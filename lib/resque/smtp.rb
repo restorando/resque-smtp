f9de4f49fc630f37f955db1b2f86bacabb0c13af
Juan Barreneche <snipperme@gmail.com>
2013-07-23 08:36:33 -0300

    Initial commit

---
+++ lib/resque/smtp.rb
@@ -0,0 +1,7 @@
+require "resque/smtp/version"
+
+module Resque
+  module Smtp
+    # Your code goes here...
+  end
+end
---
+++ lib/resque/smtp/version.rb
@@ -0,0 +1,5 @@
+module Resque
+  module Smtp
+    VERSION = "0.0.1"
+  end
+end
